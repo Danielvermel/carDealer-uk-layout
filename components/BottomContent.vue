bee61629fc7af53e7395a75bbd5bfa071b340f5a
Daniel Leite <daniel.f.m.leite@gmail.com>
2021-07-11 22:48:06 +0100

    add bottom content and representative content components

---
+++ components/BottomContent.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="flexbox-container justify-content-between align-center my-5 mx-3">
+        <a class="bottom-content-text-link flexbox-item-md-2 flexbox-item-lg-3" href="#navbar">Back to top</a>
+        <div class="my-auto flexbox-item-md-7 flexbox-item-lg-6">
+            <button class="button-type"
+                :class="{
+                    'active': carType == 'back'
+                }"
+                @click="changeResult('back')">
+                    &lsaquo;
+            </button>
+            <button class="button-type"
+                :class="{
+                    'active': carType == 'one'
+                }"
+                @click="changeResult('one')">
+                1
+            </button>
+            <button class="button-type"
+                :class="{
+                    'active': carType == 'two'
+                }"
+                @click="changeResult('two')">
+                2
+            </button>
+            <button class="button-type"
+                :class="{
+                    'active': carType == 'three'
+                }"
+                @click="changeResult('three')">
+                3
+            </button>
+            <button class="button-type border-strong"
+                :class="{
+                    'active': carType == 'next',
+
+                }"
+                @click="changeResult('next')">
+               <strong> &rsaquo;</strong>
+            </button>
+
+            <button class="button-type"
+                :class="{
+                    'active': carType == 'end'
+                }"
+                @click="changeResult('end')">
+                &rsaquo;&rsaquo;
+            </button>
+        </div>
+
+
+        <select class="sort-cars flexbox-item-lg-2">
+            <option value="lowestPrice">Lowest price</option>
+            <option value="highestPrice">Highest price</option>
+        </select>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['carQuantity'],
+    data() {
+        return {
+            carType: 'all',
+        };
+    },
+    methods: {
+        changeResult: function(value){
+            this.carType = value;
+            this.$emit('classificationData', value);
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/css/main.scss';
+
+.flexbox-container {
+    a {
+        text-decoration: underline;
+        color: $text-grey-color;
+    }
+
+    .button-type {
+        padding: 2px 25px 2px 25px;
+        border-radius: $border-radius-md;
+        cursor: pointer;
+        margin-right: 5px;
+        color: $text-grey-color;
+        border: solid 1px $border-grey-color;
+        background-color: #FFFFFF;
+        height: 30px;
+        font-size: 16px;
+    }
+
+    .active {
+        color: #FFFFFF;
+        background-color: $primary-color;
+        border: solid 1px $primary-color !important;
+    }
+
+    .border-strong{
+        border: 1px solid black;
+    }
+
+    .sort-cars{
+        float: right;
+        border-radius: $border-radius-lg;
+        background-color: $background-white-color;
+        padding: 13px;
+        border: none;
+        margin-left: auto;
+    }
+}
+</style>
---
+++ components/RepresentativeContent.vue
@@ -0,0 +1,121 @@
+<template>
+    <section class="representative-banner my-5 mx-3">
+        <div class="flexbox-container justify-content-between align-center">
+            <p class=" flexbox-item-md-12 flexbox-item-lg-9">
+                <strong>Representative example:</strong> Borrowing £11,790.00 over 4 years with a representative APR of 7.9 %, an annual interest rate of 4.08 % (Fixed) 
+                and a deposit of £500.00, the amount payable would be £273.59 per month, with a total cost of credit of £1,843.32 and a total amount payable 
+                of £13,633.32.
+            </p>
+            <div class="flexbox-item-md-12 flexbox-item-lg-3">
+                <button class="button-hidden px-3">
+                         <span v-if="!hideButtonActive">Hide breakdown </span>
+                        <ArrowUpIcon v-if="!hideButtonActive" @click="hideButtonActive = !hideButtonActive" class="button-hidden-arrow ml-3" />
+                        <span v-if="hideButtonActive">Show breakdown </span>
+                        <ArrowDownIcon v-if="hideButtonActive" @click="hideButtonActive = !hideButtonActive" class="button-hidden-arrow ml-3" />        
+                </button>
+            </div>
+        </div>
+        <div v-if="!hideButtonActive" class="flexbox-container justify-content-between mt-4">
+            <button 
+                class="flexbox-item-button pb-2" 
+                :class="{
+                    'active': active
+                }"
+                @click="active = !active"
+            >
+                Personal Contract Purchase
+            </button>
+            <button 
+                class="flexbox-item-button pb-2"
+                :class="{
+                    'active': !active
+                }"
+                 @click="active = !active"
+            >
+                Hire Purchase
+            </button>
+        </div>
+
+        <div v-if="!hideButtonActive" class="flexbox-container mt-3">
+            <FinanceCard title="48 monthly payments" text="£157.25"  class="px-1 pb-3" />
+            <FinanceCard title="Cash price" text="£9,998.00" class="px-1 pb-3"/>
+            <FinanceCard title="Customer deposit" text="£250.00" class="px-1 pb-3"/>
+            <FinanceCard title="Amount of credit" text="£11,980.75" class="px-1 pb-3"/>
+            <FinanceCard title="APR" text="7.9%" class="px-1 pb-3"/>
+            <FinanceCard title="Term" text="48 months" class="px-1 pb-3"/>
+            <FinanceCard title="Total amount payable" text="£12,163.72" class="px-1 pb-3"/>
+            <FinanceCard title="Rate of interest (fixed)" text="4.08% PA" class="px-1 pb-3"/>
+            <FinanceCard title="Annual mileage" text="8,000" class="px-1 pb-3"/>
+            <FinanceCard title="Optional financial payment" text="£3,888.04" class="px-1 pb-3"/>
+        </div>
+
+    </section>
+</template>
+
+<script>
+import ArrowUpIcon from 'vue-material-design-icons/ChevronUp.vue';
+import ArrowDownIcon from 'vue-material-design-icons/ChevronDown.vue';
+
+    export default {
+    data() {
+        return {
+            active: true,
+            hideButtonActive: false
+        };
+    },
+      components: {
+        ArrowUpIcon,
+        ArrowDownIcon
+        }
+
+    }
+</script>
+<style scpoed lang="scss">
+@import '~/assets/css/main.scss';
+
+.representative-banner{
+    background-color: $background-white-color;
+    border-radius: $border-radius-lg;
+    padding: 15px;
+    border: 1px solid $border-grey-color;
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+
+    .button-hidden {
+        background-color: #FFFFFF;
+        border-radius: $border-radius-lg;
+        border: solid 1px #FFFFFF; 
+        color: $text-grey-color;
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+        float: right;
+        padding: 20px 24px;
+
+    }
+
+    .button-hidden-arrow{
+        background-color: $border-grey-color;
+        color:#FFFFFF;
+        border-radius: $border-radius-sm;
+        cursor: pointer;
+    }
+
+    .flexbox-item-button {
+        width: 50%;
+        background-color: transparent;
+        border-top: none;
+        border-right: none;
+        border-bottom: 1px solid $border-grey-color;
+        border-left: none;
+    }
+
+    .active {
+        border-bottom:  3px  solid $primary-color;
+    }
+    
+    p {
+        line-height: 24px;
+        
+    }
+ }
+</style>
